7a9fcc5b0239988aeee4ee1a88e7d55fa5b68b01
rfkchrl <rfkchrl.03@gmail.com>
2019-05-21 12:17:27 +0700

    FIX PROBLEM REGISTER

--- application/controllers/C_auth.php
+++ application/controllers/C_auth.php
@@ -117,7 +117,7 @@ class C_auth extends CI_Controller {
             # code...
             $data['title'] = 'Authen';    
 
-            $this->load->view('templates/auth_header', $data);
+            $this->load->view('templates/auth_header', $data); 
             $this->load->view('auth/registration');
             $this->load->view('templates/auth_footer');
         } else {
@@ -128,20 +128,12 @@ class C_auth extends CI_Controller {
                 'password' => password_hash($this->input->post('password1'),
                             PASSWORD_DEFAULT),
                 'role_id' => 2,
-                'is_active' => 0,
-                'date_created' => time()
-            ];
-
-            // # Token
-            // $token = random_bytes(32);
-            // var_dump($token);
-            // die;
-
-            // //insert data ke database;
-            // $this->db->insert('user', $data);
-
-            # EMAIL SETTING REGISTRTATION 
-            $this->_sendEmail();
+                'is_active' => 1,
+                'date_created' => time()   
+                ];
+            
+            //insert data ke database;
+            $this->db->insert('user', $data);
 
             $this->session->set_flashdata('message',
             '<div class="alert alert-success" 
@@ -152,36 +144,6 @@ class C_auth extends CI_Controller {
             redirect('c_auth'); 
         }
     }
-
-    private function _sendEmail(){
-        $config = [
-            'protocol'  => 'smtp',
-            'smtp_host' => 'ssl://smtp.googlemail.com',
-            'smtp_user' => 'rifkich.03@gmail.com',
-            'smtp_pass' => 'Kakikuduaman123',
-            'smtp_port' => 465,
-            'mailtype'  => 'html',
-            'charset'   => 'utf-8',
-            'newline'   => "\r\n"
-        ];
-        # call lib 
-
-        $this->load->library('email', $config);
-        $this->email->initialize($config);
-
-        $this->email->from('rifkich.03@gmail.com', 'Rifki Chairil');
-        $this->email->to('rfkchrl.03@gmail.com');
-        $this->email->subject('Testing');
-        $this->email->message('Hello world');
-
-        if ($this->email->send()){
-            return true;
-        }else{
-            echo $this->email->print_debugger();
-            die;
-        }
-    }
- 
 
     public function logout(){
         $this->session->unset_userdata('email');
--- application/views/user/index.php
+++ application/views/user/index.php
@@ -152,7 +152,7 @@
         </div>
         </div>
 
-             <!-- Testing AJax -->
+        <!-- Testing AJax -->
             
         <!-- jQuery JS CDN -->
         <!-- <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>  -->
